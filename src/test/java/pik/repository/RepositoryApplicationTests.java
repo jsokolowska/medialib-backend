a24a8490122b1ab6fc54cdd04002818135ee4865
Turowska <alicjalom@wp.pl>
2021-03-30 14:49:14 +0200

    first test

--- src/test/java/pik/repository/RepositoryApplicationTests.java
+++ src/test/java/pik/repository/RepositoryApplicationTests.java
@@ -1,13 +1,29 @@
 package pik.repository;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class RepositoryApplicationTests {
 
+    @Autowired
+    private MockMvc mocMvc;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        this.mocMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello Word!"));
     }
 
 }
