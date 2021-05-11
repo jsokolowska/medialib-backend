ff4bb1ad48e1afa2d14486c7021684d4fcad78c9
Alicja Turowska <alicjalom@wp.pl>
2021-05-11 15:58:45 +0200

    dokończenie logowania i rejestracji

--- src/main/java/pik/repository/RepositoryApplication.java
+++ src/main/java/pik/repository/RepositoryApplication.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import org.springframework.http.MediaType;
+import pik.repository.mysqlDAOs.UserDAO;
 import pik.repository.openstack.MediaFile;
 import pik.repository.openstack.MediaFileDAO;
 import pik.repository.openstack.SwiftMediaFileDAO;
@@ -21,6 +22,8 @@ public class RepositoryApplication {
 
     private static final LoginUsers loginUsers = new LoginUsers();
     private static final MediaFileDAO mediaFileDAO = new SwiftMediaFileDAO();
+//    private static final UserDAO userDAO = new UserDAO();
+
     private static final String HEADER_TOKEN = "X-API-TOKEN";
 
     public static void main(String[] args) {
@@ -30,25 +33,26 @@ public class RepositoryApplication {
     @CrossOrigin
     @RequestMapping(value="/api/login", method=RequestMethod.POST, produces=MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity login(@RequestBody @Valid Login dane){
-        //tego nie będzie
         String email = dane.getEmail();
         String password = dane.getPassword();
-        String token = "token";
-        //obsluga logowania:
-
-        //odczytać z bazy czy jest użytkownik o podanym emailu i haśle
-
-        //jeśli jest to odkomentować to co jest niżej
-        //String token = loginUsers.addUser(email);
-
+/*        if(!userDAO.isUserExist(email)){
+            return ResponseEntity.notFound().build();
+        }
+        if(!userDAO.isPasswordMatch(email, password)){
+            return ResponseEntity.status(401).body("error password");
+        }
+*/        String token = loginUsers.addUser(email);
         return ResponseEntity.ok("{\"token\":\""+token+"\"}");
     }
 
     @CrossOrigin
     @RequestMapping(value="/api/signup", method=RequestMethod.POST, produces=MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity register(@RequestBody @Valid UserData dane){
-        //obsluga rejestracji: sprzwdzić czy istnieje, jeśli nie to dodać
-        return ResponseEntity.ok("");
+/*        if(userDAO.isUserExist(dane.getEmail())){
+            return ResponseEntity.status(401).body("just exist");
+        }
+        userDAO.insertUser(dane.getName(), dane.getSurname(), dane.getEmail(), dane.getPassword());
+*/        return ResponseEntity.ok("");
     }
 
     @CrossOrigin
