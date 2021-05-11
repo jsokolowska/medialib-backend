736a64dba2f6289ed2dd3e1835bff943a4f3d9a7
Anton <anton.masiukiewicz1@gmail.com>
2021-05-11 13:25:15 +0200

    Initial commit for working MysqlDAO

---
+++ src/main/java/pik/repository/mysqlDAOs/UserDAO.java
@@ -0,0 +1,175 @@
+package pik.repository.mysqlDAOs;
+
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+import java.sql.*;
+import java.util.HashMap;
+import java.util.ResourceBundle;
+
+
+import org.apache.commons.codec.binary.Hex;
+
+
+public class UserDAO {
+
+    private final String DRIVER_STRING = "com.mysql.cj.jdbc.Driver";
+    private final Connection connection;
+
+    public UserDAO() throws Exception {
+
+        ResourceBundle bundle = ResourceBundle.getBundle("mysql");
+        Class.forName(DRIVER_STRING);
+        this.connection = DriverManager.getConnection(
+                bundle.getString("connection_string"),
+                bundle.getString("mysql-user"),
+                bundle.getString("mysql-password")
+        );
+    }
+
+    public int getCount() throws Exception {
+        Statement statement = this.connection.createStatement();
+        String queryString = "SELECT COUNT(*) AS count FROM users";
+        ResultSet rs = statement.executeQuery(queryString);
+
+        int max = -1;
+
+        if (rs.next()) {
+            max = rs.getInt("count");
+        }
+
+        return max;
+    }
+
+    public HashMap<String, String> hashPassword(String password, String hexSalt) throws Exception {
+
+        byte[] byteSalt = Hex.decodeHex(hexSalt.toCharArray());
+
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), byteSalt, 1000, 512);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+
+        byte[] hash = factory.generateSecret(spec).getEncoded();
+
+        String strHash = Hex.encodeHexString(hash);
+        String strSalt = Hex.encodeHexString(byteSalt);
+
+        HashMap<String, String> passwStuff = new HashMap<>();
+        passwStuff.put("passwHash", strHash);
+        passwStuff.put("salt", strSalt);
+
+        return passwStuff;
+    }
+
+
+    public void insertUser(String firstName, String lastName, String email, String password) throws Exception {
+
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[32];
+        random.nextBytes(salt);
+
+        String ssalt = Hex.encodeHexString(salt);
+
+        HashMap<String, String> passwStuff = hashPassword(password, ssalt);
+        String passwordHash = passwStuff.get("passwHash");
+        String strSalt = passwStuff.get("salt");
+
+
+        String queryString = "INSERT INTO users(first_name, last_name, email, password_hash, seed) VALUES (?, ?, ?, ?, ?)";
+
+        PreparedStatement statement = connection.prepareStatement(queryString);
+        statement.setString(1, firstName);
+        statement.setString(2, lastName);
+        statement.setString(3, email);
+        statement.setString(4, passwordHash);
+        statement.setString(5, strSalt);
+
+        statement.execute();
+        statement.executeQuery("commit;");
+    }
+
+    public String getPasswordHash(String email) throws Exception {
+
+        Statement statement = this.connection.createStatement();
+        String queryString = String.format("SELECT * FROM users WHERE email = \"%s\"", email);
+        ResultSet rs = statement.executeQuery(queryString);
+
+        String hash = null;
+
+        if (rs.next()) {
+            hash = rs.getString("password_hash");
+        }
+        return hash;
+    }
+
+    public String getSalt(String email) throws Exception {
+
+        Statement statement = this.connection.createStatement();
+        String queryString = String.format("SELECT * FROM users WHERE email = \"%s\"", email);
+        ResultSet rs = statement.executeQuery(queryString);
+
+        String salt = null;
+
+        if (rs.next()) {
+            salt = rs.getString("seed");
+        }
+
+        return salt;
+    }
+
+    public String getUserName(String email) throws Exception {
+
+        Statement statement = this.connection.createStatement();
+        String queryString = String.format("SELECT * FROM users WHERE email = \"%s\"", email);
+        ResultSet rs = statement.executeQuery(queryString);
+
+        String username = null;
+
+        if (rs.next()) {
+            username = rs.getString("first_name");
+        }
+        return username;
+    }
+
+    public String getLastName(String email) throws Exception {
+
+        Statement statement = this.connection.createStatement();
+        String queryString = String.format("SELECT * FROM users WHERE email = \"%s\"", email);
+        ResultSet rs = statement.executeQuery(queryString);
+
+        String lastName = null;
+        if (rs.next()) {
+            lastName = rs.getString("last_name");
+        }
+        return lastName;
+    }
+
+
+    public void modifyUserEmail(String email, String newEmail) throws Exception {
+
+        String queryString = "UPDATE users SET email = ? WHERE email = ?";
+
+        PreparedStatement statement = connection.prepareStatement(queryString);
+        statement.setString(1, newEmail);
+        statement.setString(2, email);
+
+        statement.execute();
+
+    }
+
+    public void deleteUser(String email) throws Exception {
+
+        String queryString = String.format("DELETE FROM users WHERE email = \"%s\"", email);
+        Statement statement = this.connection.createStatement();
+        statement.execute(queryString);
+        statement.execute("commit;");
+    }
+
+    public void close() throws Exception {
+        this.connection.close();
+    }
+
+}
---
+++ src/test/java/pik/repository/mysqlDAO/UserDAOSpec.groovy
@@ -0,0 +1,95 @@
+package pik.repository.mysqlDAO
+
+import pik.repository.mysqlDAOs.UserDAO
+import spock.lang.Specification
+
+class UserDAOSpec extends Specification {
+
+    def "Should return proper name"() {
+        given:
+        def userDao = new UserDAO()
+
+        when:
+        def name = userDao.getUserName("ptoplis0@home.pl")
+
+        then:
+        name == "Peggie"
+
+    }
+
+    def "Creating user test"() {
+        given:
+        def userDao = new UserDAO()
+        def initialCount = userDao.getCount()
+
+        when:
+        userDao.insertUser(null, "Turing", "alan.turing@gmail.com", "ImmaAlan")
+        def lastName = userDao.getLastName("alan.turing@gmail.com")
+        def modifiedCount = userDao.getCount()
+
+
+        then:
+        lastName == "Turing"
+        modifiedCount == initialCount + 1
+
+    }
+
+    def "Updating email test"() {
+        given:
+        def userDao = new UserDAO()
+        def initialName = userDao.getUserName("ada.lovelace@gmail.com")
+
+
+        when:
+        userDao.modifyUserEmail("ada.lovelace@gmail.com", "a.da.lovelace@gmail.com")
+        def newEmailName = userDao.getUserName("a.da.lovelace@gmail.com")
+
+        then:
+        initialName == newEmailName
+
+    }
+
+    def "modify user back"() {
+        given:
+        def userDao = new UserDAO()
+        def initialName = userDao.getUserName("a.da.lovelace@gmail.com")
+
+        when:
+        userDao.modifyUserEmail("a.da.lovelace@gmail.com", "ada.lovelace@gmail.com")
+        def newEmailName = userDao.getUserName("ada.lovelace@gmail.com")
+
+        then:
+        initialName == newEmailName
+    }
+
+    def "Deleting test"() {
+        given:
+        def userDao = new UserDAO()
+        def initialCount = userDao.getCount()
+
+        when:
+        userDao.deleteUser("alan.turing@gmail.com")
+        def username = userDao.getUserName("alan.turing@gmail.com")
+        def newCount = userDao.getCount()
+
+        then:
+        username == null
+        newCount == initialCount - 1
+
+    }
+
+    def "Test password matching"() {
+        given:
+        def userDao = new UserDAO()
+        def userPasswHash = userDao.getPasswordHash("bfend9@gmpg.org")
+        def userSalt = userDao.getSalt("bfend9@gmpg.org")
+
+        when:
+        def userPasswStuff = userDao.hashPassword("77Ila32PlRdi", userSalt)
+
+        then:
+        System.out.println(userSalt)
+        userPasswStuff.get("passwHash") == userPasswHash
+        userPasswStuff.get("salt") == userSalt
+    }
+}
