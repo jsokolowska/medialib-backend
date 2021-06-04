999445ef7c9bae4433b7d4c8b611a1b5fc5123fc
Turowska <alicjalom@wp.pl>
2021-06-04 22:53:07 +0200

    naprawa widzenia testow

--- src/test/java/pik/repository/RepositoryApplicationTest.java
+++ src/test/java/pik/repository/RepositoryApplicationTest.java
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class RepositoryApplicationTestsNotSpock {
+class RepositoryApplicationTest {
 
     @Autowired
     private MockMvc mocMvc;
