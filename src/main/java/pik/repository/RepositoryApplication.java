3d9d10b5deab600f24c410a038d9154c4f96f391
jsokolowska <joana.sokolowska@gmail.com>
2021-06-09 10:58:25 +0200

    Hotfix 2.0

--- src/main/java/pik/repository/RepositoryApplication.java
+++ src/main/java/pik/repository/RepositoryApplication.java
@@ -2,21 +2,34 @@ package pik.repository;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.javaswift.joss.client.factory.AccountConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import pik.repository.mysqlDAOs.UserDAO;
-import pik.repository.openstack.MediaFileDAO;
-import pik.repository.openstack.SwiftMediaFileDAO;
-import pik.repository.util.*;
 
 import javax.validation.Valid;
+import javax.xml.ws.Response;
+
+import org.springframework.http.MediaType;
+import pik.repository.mysqlDAOs.UserDAO;
+import pik.repository.util.*;
+import pik.repository.openstack.MediaFileDAO;
+import pik.repository.openstack.SwiftMediaFileDAO;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
 
